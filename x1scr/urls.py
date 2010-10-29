6c75ac632cc273a46e1fb646f7208b7c7d1eb911
Aleksey Radchenko <dixon.che@gmail.com>
2010-10-29 13:40:25 +0300

    added index view

--- x1scr/urls.py
+++ x1scr/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', "x1scr.views.index", name="index"),
     (r'^admin/', include(admin.site.urls)),
 )
