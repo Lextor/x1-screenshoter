8be4cb148acc31664a0602cb5e67bdf5c52ab1f7
Oksana Slusarenko <oksana.slu@gmail.com>
2010-11-12 08:35:15 +0200

    form processing

--- x1scr/apps/inbox/models.py
+++ x1scr/apps/inbox/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from django.forms import ModelForm, Textarea
 
-import datetime
-
 
 class ContactFile(models.Model):
     email = models.EmailField(max_length=75)
     name = models.CharField(blank=True, max_length=100)
     subject = models.CharField(max_length=200)
     text = models.TextField()
-    timestamp = models.DateField(auto_now_add=False)
+    timestamp = models.DateField()
     comment = models.TextField(blank=True)
 
     def __unicode__(self):
--- x1scr/apps/inbox/views.py
+++ x1scr/apps/inbox/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from x1scr.apps.inbox.models import ContactForm
+from django.http import HttpResponseRedirect, HttpResponse
+from datetime import datetime
+from x1scr.apps.inbox.models import ContactForm, ContactFile
 
 
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            cleaned_data = form.cleaned_data
+            now_date = datetime.now()
+            Contact_object = ContactFile(email=cleaned_data['email'],
+                                         name=cleaned_data['name'],
+                                         subject=cleaned_data['subject'],
+                                         text=cleaned_data['text'],
+                                         timestamp=now_date)
+            Contact_object.save()
+            request.user.message_set.create(message="Message successfully sent")
 
-            return HttpResponseRedirect('/thanks/')
+            return HttpResponseRedirect('/contact/')
     else:
         form = ContactForm()
 
