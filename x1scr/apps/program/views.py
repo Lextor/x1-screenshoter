e37490455bcf75752907eaec6c98e9bdee83b326
Aleksey Radchenko <dixon.che@gmail.com>
2010-11-20 23:01:37 +0200

    add download page

--- x1scr/apps/program/views.py
+++ x1scr/apps/program/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect, HttpResponse
-from datetime import datetime
-#from x1scr.apps.program.models import 
+#from django.http import HttpResponseRedirect, HttpResponse
+#from datetime import datetime
+from x1scr.apps.program.models import Program
 
 
 def download(request):
+    programs = Program.objects.filter(published=True)
     return render_to_response('download.html',
-                              context_instance=RequestContext(request, {}))
-
+                              context_instance=RequestContext(request,
+                                                              dict(programs=programs)))
