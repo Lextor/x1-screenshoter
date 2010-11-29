a35d62611444203aa7f217eb49e32e8bc50c3aea
Oksana Slusarenko <oksana.slu@gmail.com>
2010-11-29 14:44:49 +0200

    added 'Terms of Use'

--- x1scr/views.py
+++ x1scr/views.py
@@ -33,6 +33,13 @@ def features(request):
                               context_instance=RequestContext(request, dict(flatpage=flatpage)))
 
 
+def termsofuse(request):
+    url_path = request.META['PATH_INFO']
+    flatpage = get_object_or_404(FlatPage, url=url_path)
+    return render_to_response('flatpages/about.html',
+                              context_instance=RequestContext(request, dict(flatpage=flatpage)))
+
+
 def message(request):
     return render_to_response('message.html',
                               context_instance=RequestContext(request, dict()))
